cc8accd4732403efb9049c85168bdbd5c5c595f4
kekkerslawl <braydj@gmail.com>
2021-07-04 19:16:46 -0500

    react-router-bootstrap testing

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,39 +1,51 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
-  return (
-    <Navbar className="navbar bg-usftheme navbar-dark" expand="lg">
-      <Navbar.Brand href="/announcements">USF Driver Tools</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/announcements">Announcements</Nav.Link>
-          <NavDropdown title="Locations" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/location-search">
-              Search Locations
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/location-add">
-              Add Location
-            </NavDropdown.Item>
-          </NavDropdown>
-          <NavDropdown title="Feedback" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/feedback-routing">
-              Routing
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/feedback-loading">
-              Loading
-            </NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="/barcode-generator">Barcode Generator</Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#profile">Profile</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+	return (
+		<Navbar className="navbar bg-usftheme navbar-dark" expand="lg">
+			<LinkContainer to="/announcements">
+				<Navbar.Brand>USF Driver Tools</Navbar.Brand>
+			</LinkContainer>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<LinkContainer to="/announcements">
+						<Nav.Link>Announcements</Nav.Link>
+					</LinkContainer>
+					<NavDropdown title="Locations" id="basic-nav-dropdown">
+						<LinkContainer to="/location-search">
+							<NavDropdown.Item>
+								Search Locations
+							</NavDropdown.Item>
+						</LinkContainer>
+						<LinkContainer to="/location-add">
+							<NavDropdown.Item>Add Location</NavDropdown.Item>
+						</LinkContainer>
+					</NavDropdown>
+					<NavDropdown title="Feedback" id="basic-nav-dropdown">
+						<LinkContainer to="/feedback-routing">
+							<NavDropdown.Item>Routing</NavDropdown.Item>
+						</LinkContainer>
+
+						<LinkContainer to="/feedback-loading">
+							<NavDropdown.Item>Loading</NavDropdown.Item>
+						</LinkContainer>
+					</NavDropdown>
+					<LinkContainer to="/barcode-generator">
+						<Nav.Link>Barcode Generator</Nav.Link>
+					</LinkContainer>
+				</Nav>
+				<Nav>
+					<LinkContainer to="/profile">
+						<Nav.Link>Profile</Nav.Link>
+					</LinkContainer>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
 
 export default Header;
