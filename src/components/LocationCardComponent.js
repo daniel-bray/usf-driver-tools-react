3af83e65623a68938340f2114243516efbc0f59f
kekkerslawl <braydj@gmail.com>
2021-07-06 21:39:32 -0500

    added filtering for stops in search bar

--- src/components/LocationCardComponent.js
+++ src/components/LocationCardComponent.js
@@ -2,38 +2,42 @@ import React, { Component } from "react";
 import { LOCATIONS } from "../shared/locations";
 import { Row, Col, Card, Button } from "react-bootstrap";
 
-function RenderLocation({ location }) {
-  return (
-    <Col xs={12} md={6} lg={4}>
-      <Card style={{ marginBottom: "2rem" }}>
-        <Card.Img variant="top" src={location.image} />
-        <Card.Body>
-          <Card.Title>{location.stopName}</Card.Title>
-          <Card.Text>
-            {location.city}, {location.state}
-          </Card.Text>
-          <Button className="btn btn-usf-green" variant="success">
-            View Stop
-          </Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
+function RenderLocation({ stop }) {
+	return (
+		<Col xs={12} md={6} lg={4}>
+			<Card style={{ marginBottom: "2rem" }}>
+				<Card.Img variant="top" src={stop.image} />
+				<Card.Body>
+					<Card.Title>{stop.stopName}</Card.Title>
+					<Card.Text>
+						{stop.city}, {stop.state}
+					</Card.Text>
+					<Button className="btn btn-usf-green" variant="success">
+						View Stop
+					</Button>
+				</Card.Body>
+			</Card>
+		</Col>
+	);
 }
 
 class LocationCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locations: LOCATIONS,
-    };
-  }
-  render() {
-    const LocationList = this.state.locations.map((location) => {
-      return <RenderLocation location={location} />;
-    });
-    return <Row>{LocationList}</Row>;
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			locations: LOCATIONS,
+		};
+	}
+	render() {
+		const stops = this.state.locations.filter((location) =>
+			location.stopName.toLowerCase().includes(this.props.location)
+		);
+
+		const LocationList = stops.map((stop) => {
+			return <RenderLocation stop={stop} />;
+		});
+		return <Row>{LocationList}</Row>;
+	}
 }
 
 export default LocationCard;
--- src/components/LocationComponent.js
+++ src/components/LocationComponent.js
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
+
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import LocationCard from "./LocationCardComponent";
+
 function Location() {
-  return (
-    <Container className="mt-3">
-      <InputGroup className="mb-3">
-        <InputGroup.Prepend>
-          <InputGroup.Text id="location-input">Location Name</InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl
-          placeholder=""
-          aria-label=""
-          aria-describedby="location-input"
-        />
-      </InputGroup>
-      <LocationCard />
-    </Container>
-  );
+	const [location, setLocation] = useState("");
+
+	return (
+		<Container className="mt-3">
+			<InputGroup
+				className="mb-3"
+				onChange={(e) => setLocation(e.target.value)}
+			>
+				<InputGroup.Prepend>
+					<InputGroup.Text id="location-input">
+						Location Name
+					</InputGroup.Text>
+				</InputGroup.Prepend>
+				<FormControl
+					placeholder=""
+					aria-label=""
+					aria-describedby="location-input"
+				/>
+			</InputGroup>
+			<LocationCard location={location} />
+		</Container>
+	);
 }
 
 export default Location;
